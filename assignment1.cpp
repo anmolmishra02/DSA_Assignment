23b88fc2663d887c4e2453953472db193125b6e2
Anmol Mishra <52851481+anmolmishra02@users.noreply.github.com>
2023-05-31 20:53:17 +0530

    Update assignment1.cpp

--- assignment1.cpp
+++ assignment1.cpp
@@ -232,6 +232,33 @@ int main()
 
 
 
+/*==================================================================================================================
+Q-7
+*/
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void pushZerosToEnd(int arr[], int n)
+{
+    int count = 0;  
+    for (int i = 0; i < n; i++)
+        if (arr[i] != 0)
+            arr[count++] = arr[i]; 
+    while (count < n)
+        arr[count++] = 0;
+}
+ 
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++)
+       cin>>arr[i];
+    pushZerosToEnd(arr, n);
+    return 0;
+
+
 /*==============================================================================================================================
 Q8*/
 
