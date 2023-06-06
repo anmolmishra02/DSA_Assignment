1c815202483395eae2129d11de9dcc4b35102d9f
Anmol Mishra <52851481+anmolmishra02@users.noreply.github.com>
2023-06-07 00:39:12 +0530

    Update Assignment9.cpp

--- Assignment9.cpp
+++ Assignment9.cpp
@@ -7,3 +7,25 @@ public:
      return (n==1 or (n%2==0 and isPowerOfTwo(n/2)));   
     }
 };
+
+
+//Question 2
+
+#include<bits/stdc++.h>
+using namespace std;
+int sumofnatural(int n)
+{
+    if(n==0)
+      return 0;
+    return n+sumofnatural(n-1);
+    
+}
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<sumofnatural(n);
+}
+
+
+//Question 3
