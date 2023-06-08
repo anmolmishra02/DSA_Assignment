b58d92cf87df2e6d88fcf3c5b425bebdaa812037
Anmol Mishra <52851481+anmolmishra02@users.noreply.github.com>
2023-06-09 02:00:30 +0530

    Update Assignment9.cpp

--- Assignment9.cpp
+++ Assignment9.cpp
@@ -122,6 +122,29 @@ int main()
 }
 
 
+//-------------------------------------------Question 7
+#include<bits/stdc++.h>
+using namespace std;
+
+void permute(string &a,int l,int r)
+{
+    if(l==r)
+      cout<<a<<",";
+    for(int i=l;i<=r;i++)
+    {
+        swap(a[l],a[i]);
+        permute(a,l+1,r);
+        swap(a[l],a[i]);
+    }
+}
+
+int main()
+{
+   string s;
+   cin>>s;
+   permute(s,0,s.size()-1);
+}
+
 
 
 //-------------------------------------------Question 8
