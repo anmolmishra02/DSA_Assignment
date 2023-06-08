1d4e4c51c6ff3ef2f928d00835f95993554ac7a2
Anmol Mishra <52851481+anmolmishra02@users.noreply.github.com>
2023-06-08 19:29:46 +0530

    Update Assignment9.cpp

--- Assignment9.cpp
+++ Assignment9.cpp
@@ -98,3 +98,32 @@ int main()
     int ans=getmaximum(arr,n-1);
     cout<<ans;
 }
+
+
+
+
+//////////Question 8
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int productarr(vector<int>&arr,int n)
+{
+    if(n==0)
+    {
+        return arr[0];
+    }
+    return arr[n]*productarr(arr,n-1);
+}
+int main()
+{
+    int n;
+    cin>>n;
+    vector<int>arr(n,0);
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int getproduct=productarr(arr,n-1);
+    cout<<getproduct;
+}
